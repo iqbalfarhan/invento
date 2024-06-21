9e4b3ca145c94a25cb0abae88283d96d859c1190
IQBAL FARHAN SYUHADA <iqbalfarhan1996@gmail.com>
2024-06-21 19:11:21 +0800

    first init

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,40 @@
+import { LogOut, UserCircle, LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+import { cloneElement } from "react";
+
+const Item = ({ label = "Menu label", href = "/", icon }) => {
+    const newicon = cloneElement(icon, { className: "size-5" });
+    return (
+        <li>
+            <Link href={href}>
+                {newicon}
+                <span>{label}</span>
+            </Link>
+        </li>
+    );
+};
+
+const Group = ({ title = "Menu label", children }) => {
+    return (
+        <li>
+            <h2 className="menu-title">{title}</h2>
+            <ul>{children}</ul>
+        </li>
+    );
+};
+
+const Sidebar = () => {
+    return (
+        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content space-y-6">
+            <Group title="Dashboard">
+                <Item label="Dashboard" href="/" icon={<LayoutDashboard />} />
+            </Group>
+            <Group title="Edit Profile">
+                <Item label="Tentang Aplikasi" href="/" icon={<UserCircle />} />
+                <Item label="Keluar Aplikasi" href="/" icon={<LogOut />} />
+            </Group>
+        </ul>
+    );
+};
+
+export default Sidebar;
